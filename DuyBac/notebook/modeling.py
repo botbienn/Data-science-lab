f3c52787045849d5e87ba5a68563144fbeae1d28
botbien <vubac2004@gmail.com>
2024-12-05 14:54:26 +0700

    get corr matrix

--- DuyBac/notebook/modeling.py
+++ DuyBac/notebook/modeling.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pandas.core.array_algos.replace import compare_or_regex_search
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -18,17 +19,26 @@ def get_file_list():
     result = [f[:-4] for f in os.listdir(directory)]
     return result 
 
-LOCATIONS_LIST = get_file_list()
 
 def read_location_data(loc_list): 
-    result = [pd.read_csv(f'../data/{loc}.csv') 
-                             for loc in loc_list]
+    result = [pd.read_csv(f'../data/{loc}.csv') for loc in loc_list]
     return result
 
-pd_list = read_location_data(LOCATIONS_LIST) 
-
-print(pd_list[0].head())
 
 '''------------------------------------------------------------------------'''
+# Phân tích lựa chọn các thuộc tính cần để sử dụng cho model
+
+def extract_corr_matrix(df_s: list):
+    result = [df.select_dtypes(exclude='object').corr() 
+        for df in df_s]    
+    return result
+
+'''------------------------------------------------------------------------'''
+LOCATIONS_LIST = get_file_list()
+
+df_list = read_location_data(LOCATIONS_LIST) 
+corr_mats = extract_corr_matrix(df_list)
+
+print(corr_mats[0])
 
 print('end')
